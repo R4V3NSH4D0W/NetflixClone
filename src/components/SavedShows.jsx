d715037011170b7584cddd367b07d031cda45dd5
R4V3NSH4D0W <88871970+R4V3NSH4D0W@users.noreply.github.com>
2023-04-30 14:15:08 +0545

    added infoModel

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -4,7 +4,7 @@ import { useState,useEffect } from 'react';
 import { UserAuth } from '../context/AuthContext';
 import { db } from '../firebase';
 import {updateDoc,doc,onSnapshot} from 'firebase/firestore'
-
+import {AiOutlineClose} from 'react-icons/ai'
 const SavedShows = () => {
     const [movies, setMovies] = useState([]);
     const { user } = UserAuth();
@@ -22,6 +22,18 @@ const SavedShows = () => {
           setMovies(doc.data()?.savedShows);
         });
       }, [user?.email]);
+      const movieRef =doc(db, 'users',`${user?.email}`)
+      const deleteShow = async(passedID)=>{
+        try{
+            const result =movies.filter((item)=>item.id!==passedID)
+            await updateDoc(movieRef,{
+                savedShows:result,
+            })
+            
+        }catch(error){
+            console.log(error)
+        }
+      }
   return (
     <>
           <h2 className='text-white font-bold md:text-xl p-4'>My Shows</h2>
@@ -36,6 +48,7 @@ const SavedShows = () => {
              <img className=' w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.img}`} alt={item?.title}/>
              <div className=' absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
                  <p className=' whitespace-normal text-xl md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
+                 <p className=' absolute text-gray-300 top-4  right-4' onClick={()=>deleteShow(item.id)}><AiOutlineClose/></p>
                  </div>
                  </div>
           ))}
