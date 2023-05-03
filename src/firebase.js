2f1b3ef6377b23801ddc039c9aa7134fb4745b01
R4V3NSH4D0W <88871970+R4V3NSH4D0W@users.noreply.github.com>
2023-05-03 12:32:14 +0545

    fixed

--- src/firebase.js
+++ src/firebase.js
@@ -6,21 +6,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDu8GApOt_jKC742n2mY_vhBV9UQUSG0O4",
-  authDomain: "netflix-react-yt-f8833.firebaseapp.com",
-  projectId: "netflix-react-yt-f8833",
-  storageBucket: "netflix-react-yt-f8833.appspot.com",
-  messagingSenderId: "184848749121",
-  appId: "1:184848749121:web:148bcfcfec1001a731acc0",
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER,
+  appId: import.meta.env.VITE_APP_APP_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
-//   appId: process.env.REACT_APP_APP_ID,
